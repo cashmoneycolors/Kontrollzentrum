95c717c6f4a0d20f549297373e1203740e767ba9
Developer <dev@kontrollzentrum.de>
2025-12-09 00:45:40 +0100

    ✅ GitHub Ready - All Structure & Documentation Complete

--- obj/Debug/net8.0-windows/KontrollzentrumDesktop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/KontrollzentrumDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ea48ad8771be2c825f077ddaf1c774c382088d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753efd6feec93c4a5f47bf599f904b7e595f4ed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
