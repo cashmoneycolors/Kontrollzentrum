ed769c9fc18768085eede0fa49de9eefbd0cc818
Developer <dev@kontrollzentrum.de>
2025-12-13 06:56:00 +0100

    🔄 Auto-Migration 2025-12-13 06:56

--- obj/Debug/net8.0-windows/KontrollzentrumDesktop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/KontrollzentrumDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33907d6c1dbca2fc181e3459b11e3feecab85684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf128d21d9085cd6116f82db672eb8a03ad62ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kontrollzentrum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
